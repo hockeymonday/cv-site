e56c71342ca21752a99d99b7413842f5ca786d15
Adam Kadhim <adamakadhim@gmail.com>
2020-08-21 14:23:14 -0400

    code init

---
+++ js/function.js
@@ -0,0 +1,29 @@
+const listItems = document.querySelectorAll(".main li");
+const allimages = document.querySelectorAll(".images");
+
+function toggleActiveClass(active) {
+  listItems.forEach((item) => {
+    item.classList.remove("active");
+  });
+  active.classList.add("active");
+}
+
+function toggleimages(dataClass) {
+  if (dataClass === "all") {
+    for (let i = 0; i < allimages.length; i++) {
+      allimages[i].style.display = "block";
+    }
+  } else {
+    for (let i = 0; i < allimages.length; i++)
+      allimages[i].dataset.class === dataClass
+        ? (allimages[i].style.display = "block")
+        : (allimages[i].style.display = "none");
+  }
+}
+
+for (let i = 0; i < listItems.length; i++) {
+  listItems[i].addEventListener("click", function () {
+    toggleActiveClass(listItems[i]);
+    toggleimages(listItems[i].dataset.class);
+  });
+}
